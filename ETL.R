bdc45104e895ddcad696ee81af1313f2c042395c
rafael747cardoso <rafael747cardoso@gmail.com>
2022-02-20 02:32:09 -0300

    etl

---
+++ ETL.R
@@ -0,0 +1,31 @@
+
+#######################################################################################################################
+############################################### Exoplanets ETL ########################################################
+#######################################################################################################################
+
+# Paths:
+path_funcs = "funcs/"
+path_data = "data/"
+path_lib = "renv/library/R-4.1/x86_64-pc-linux-gnu/"
+
+# Packages:
+require(package = "dplyr", lib.loc = path_lib)
+require(package = "plotly", lib.loc = path_lib)
+require(package = "stringr", lib.loc = path_lib)
+
+# Functions:
+# source(paste0(path_funcs, "func1.R"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
